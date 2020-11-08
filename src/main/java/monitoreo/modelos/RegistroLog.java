f355060e6311c45fda203315a9ac83309e4658e3
Gianmar <gianmar.sanchez@gmail.com>
2020-11-08 11:31:58 -0500

    Prototype and Singleton, button center

---
+++ src/main/java/monitoreo/modelos/RegistroLog.java
@@ -0,0 +1,19 @@
+package monitoreo.modelos;
+
+public class RegistroLog {
+
+    public static RegistroLog registro;
+
+    public static synchronized RegistroLog getInstance()  {
+
+        if (registro == null)    {
+            registro = new RegistroLog();
+        }
+        return registro;
+    }
+
+    public void log (String mensaje)    {
+
+        System.out.println( mensaje );
+    }
+}
--- src/main/java/monitoreo/modelos/Ventana.java
+++ src/main/java/monitoreo/modelos/Ventana.java
@@ -1,11 +1,18 @@
 package monitoreo.modelos;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Ventana extends Application {
+
+    private Mapa mapaBase;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -22,8 +29,39 @@ public class Ventana extends Application {
         stage.setScene(scene);
 
         // create a MapView to display the map and add it to the stack pane
-        stackPane.getChildren().add(new Mapa().getMapView());
+        mapaBase = new Mapa();
+        mapaBase.imprimeCoordenadasActual();
+        stackPane.getChildren().add(mapaBase.getMapView());
 
+        Button btnNuevo = new Button();
+        btnNuevo.setText("Nuevo");
+        btnNuevo.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                muestraNuevaVentana();
+            }
+        });
+
+        // https://developers.arcgis.com/java/latest/java/sample-code/change-viewpoint/
+        stackPane.getChildren().add(btnNuevo);
+        stackPane.setAlignment(btnNuevo, Pos.BOTTOM_CENTER);
+        stackPane.setMargin(btnNuevo, new Insets(10, 10, 10, 10));
     }
+
+    public void muestraNuevaVentana() {
+        Stage stage = new Stage();
+        StackPane stackPane = new StackPane();
+        Scene scene = new Scene(stackPane);
+        stage.setScene(scene);
+
+        //  Clonacion de MapaBase
+        Mapa mapaBase2 = (Mapa)mapaBase.copiar();
+
+        mapaBase2.imprimeCoordenadasActual();
+        stackPane.getChildren().add(mapaBase2.getMapView());
+
+        stage.show();
+    }
+
 
 }
